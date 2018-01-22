992af58e74e484c0b48a81b35cab2c361a365332
BarningDevelopment <barningdevelopment@gmail.com>
2018-01-22 16:37:10 +0100

    wifi bijna werkend

--- Communication Manager/Window1.xaml.cs
+++ Communication Manager/Window1.xaml.cs
@@ -2,8 +2,8 @@
 using System.Windows;
 using System.Windows.Controls;
 using NativeWifi;
-
-
+using System.Runtime.InteropServices;
+using SimpleWifi;
 
 namespace Communication_Manager
 {
@@ -12,8 +12,7 @@ namespace Communication_Manager
     /// </summary>
     public partial class Window1 : Window
     {
-        private static Wifi wifi;
-
+       
         public Window1()
         {
             InitializeComponent();
@@ -21,7 +20,7 @@ namespace Communication_Manager
 
         private void checkBox1_Checked(object sender, RoutedEventArgs e)
         {
-
+            
         }
 
 
@@ -30,27 +29,151 @@ namespace Communication_Manager
 
 
         //wifi elements
-        private void checkBox_Checked(object sender, RoutedEventArgs e)
+        private void wifi_checkBox_Checked(object sender, RoutedEventArgs e)
         {
-            if (checkBox.IsChecked == true)
+            if (wifi_checkBox.IsChecked == true)
+            {             
+
+                //punt info in list grid
+                var gridView = new GridView();
+                this.wifi_listView.View = gridView;
+                gridView.Columns.Add(new GridViewColumn
+                {
+                    Header = "Accesspoints",
+                    DisplayMemberBinding = new System.Windows.Data.Binding("AccesPoint")
+                });
+                gridView.Columns.Add(new GridViewColumn
+                {
+                    Header = "Signal Strength",
+                    DisplayMemberBinding = new System.Windows.Data.Binding("Signal")
+                });
+
+                gridView.Columns.Add(new GridViewColumn
+                {
+                    Header = "BSSType",
+                    DisplayMemberBinding = new System.Windows.Data.Binding("BSSType")
+                });
+                gridView.Columns.Add(new GridViewColumn
+                {
+                    Header = "MAC",
+                    DisplayMemberBinding = new System.Windows.Data.Binding("MAC")
+                });
+                gridView.Columns.Add(new GridViewColumn
+                {
+                    Header = "RSSID",
+                    DisplayMemberBinding = new System.Windows.Data.Binding("RSSID")
+                });
+
+                WlanClient client = new WlanClient();
+
+                try
+                {
+                    wifi_password_label.Content = "";
+                    foreach (WlanClient.WlanInterface wlanIface in client.Interfaces)
+                    {
+
+                        Wlan.WlanBssEntry[] wlanBssEntries = wlanIface.GetNetworkBssList();
+
+                        foreach (Wlan.WlanBssEntry network in wlanBssEntries)
+                        {                            
+
+                            int rss = network.rssi;
+                            //     MessageBox.Show(rss.ToString());
+                            byte[] macAddr = network.dot11Bssid;
+
+                            string tMac = "";
+
+                            for (int i = 0; i < macAddr.Length; i++)
+                            {
+
+                                tMac += macAddr[i].ToString("x2").PadLeft(2, '0').ToUpper();
+                            }
+                            Wlan.Dot11Ssid ssid = network.dot11Ssid;
+
+                            string networkName = System.Text.Encoding.ASCII.GetString(ssid.SSID, 0, (int)ssid.SSIDLength);
+                            uint signal = network.linkQuality;
+                            string bssType = network.dot11BssType + "la";
+                            string MAC = tMac;
+                            string RSSID = rss.ToString();
+
+
+                            // Populate list
+                            this.wifi_listView.Items.Add(new wifiConnect { AccesPoint = networkName, Signal = signal, BSSType = bssType, MAC = MAC, RSSID = RSSID });
+                                                
+                        }
+                        wifiConnect selectedItem = (wifiConnect)wifi_listView.SelectedItem;
+                       
+                        if (selectedItem !=null)
+                        {
+                            
+                            wifi_password_label.Content = "AccesPoint is selected" + selectedItem.AccesPoint;
+                        }else
+                        {
+                            wifi_password_label.Content = "Please select an AccesPoint" ;
+                        }
+                    }
+
+                }
+                catch (System.Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                            
+            }else if (wifi_checkBox.IsChecked == false)
             {
-            }
-               
+                wifi_password_label.Content = "Please select connection type";
+            }                      
+            
         }
 
 
-        private void wifi_listView_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-           }
+        
         private void wifi_connect_button_Click(object sender, RoutedEventArgs e)
         {
+            wifi_password_label.Content = "Connet to wifi AP...";
+           
 
+            if (wifi_listView.SelectedItems.Count > 0 )
+            {
+                wifiConnect selectedItem = (wifiConnect)wifi_listView.SelectedItem;
+                MessageBox.Show(selectedItem.AccesPoint);
+
+                ListView selectAp = new ListView();
+                selectAp.Items.Add(selectedItem.AccesPoint);
+                SimpleWifi.AccessPoint ap = (AccessPoint)selectAp.Tag;
+
+                if (ConnectToWifi(ap, wifi_passwordBox.Password))
+                {
+                    wifi_password_label.Content = "You are now connected to the internet";
+
+                }
+                else
+                {
+                    wifi_password_label.Content = "Something went wrong try again";
+                }
+            }
+            else
+            {
+                wifi_password_label.Content = "Please select a network";
+            }
             
             
+
         }
 
-       
+        private bool ConnectToWifi(SimpleWifi.AccessPoint ap, string password)
+        {
+            SimpleWifi.AuthRequest authRequest = new SimpleWifi.AuthRequest(ap);
+           authRequest.Password = wifi_passwordBox.Password;
+
+            return ap.Connect(authRequest);
+
+        }
+        private void wifi_listView_SelectionChanged(object sender, System.EventArgs e)
+        {
+
+
+        }
 
         private void wifi_password_TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
@@ -58,15 +181,15 @@ namespace Communication_Manager
         }
 
 
+
+
+
         //mobile elements
-        private void checkBox1_Copy_Checked(object sender, RoutedEventArgs e)
+        private void mobile_checkBox_Checked(object sender, RoutedEventArgs e)
         {
-            if (checkBox.IsChecked == true)
+            if (mobile_checkBox.IsChecked == true)
             {
-                checkBox1_Copy.IsChecked = false;
-
                 mobile_password_label.Content = "mobile connections are beeing collected......";
-
             }
         }
 
@@ -84,5 +207,9 @@ namespace Communication_Manager
         {
         }
 
+        private void wifi_listView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
---
+++ Communication Manager/wifiConnect.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using NativeWifi;
+
+namespace Communication_Manager
+{
+    class wifiConnect
+    {
+        
+
+public static class WlanRadio
+    {
+        public static string[] GetInterfaceNames()
+        {
+            using (var client = new WlanClient())
+            {
+                return client.Interfaces.Select(x => x.InterfaceName).ToArray();
+            }
+        }
+
+        public static bool TurnOn(string interfaceName)
+        {
+            var interfaceGuid = GetInterfaceGuid(interfaceName);
+            if (!interfaceGuid.HasValue)
+                return false;
+
+            return SetRadioState(interfaceGuid.Value, Wlan.Dot11RadioState.On);
+        }
+
+        public static bool TurnOff(string interfaceName)
+        {
+            var interfaceGuid = GetInterfaceGuid(interfaceName);
+            if (!interfaceGuid.HasValue)
+                return false;
+
+            return SetRadioState(interfaceGuid.Value, Wlan.Dot11RadioState.Off);
+        }
+
+        private static Guid? GetInterfaceGuid(string interfaceName)
+        {
+            using (var client = new WlanClient())
+            {
+                return client.Interfaces.FirstOrDefault(x => x.InterfaceName == interfaceName)?.InterfaceGuid;
+            }
+        }
+
+        private static bool SetRadioState(Guid interfaceGuid, Wlan.Dot11RadioState radioState)
+        {
+            var state = new Wlan.WlanPhyRadioState
+            {
+                dwPhyIndex = (int)Wlan.Dot11PhyType.Any,
+                dot11SoftwareRadioState = radioState,
+            };
+            var size = Marshal.SizeOf(state);
+
+            var pointer = IntPtr.Zero;
+            try
+            {
+                pointer = Marshal.AllocHGlobal(size);
+                Marshal.StructureToPtr(state, pointer, false);
+
+                var clientHandle = IntPtr.Zero;
+                try
+                {
+                    uint negotiatedVersion;
+                    var result = Wlan.WlanOpenHandle(
+                        Wlan.WLAN_CLIENT_VERSION_LONGHORN,
+                        IntPtr.Zero,
+                        out negotiatedVersion,
+                        out clientHandle);
+                    if (result != 0)
+                        return false;
+
+                    result = Wlan.WlanSetInterface(
+                        clientHandle,
+                        interfaceGuid,
+                        Wlan.WlanIntfOpcode.RadioState,
+                        (uint)size,
+                        pointer,
+                        IntPtr.Zero);
+
+                    return (result == 0);
+                }
+                finally
+                {
+                    Wlan.WlanCloseHandle(
+                        clientHandle,
+                        IntPtr.Zero);
+                }
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(pointer);
+            }
+        }
+
+        public static string[] GetAvailableNetworkProfileNames(string interfaceName)
+        {
+                string[] emptyStringArray = new string[0];
+                using (var client = new WlanClient())
+            {
+                var wlanInterface = client.Interfaces.FirstOrDefault(x => x.InterfaceName == interfaceName);
+                if (wlanInterface == null)
+                       
+                    return emptyStringArray;
+
+                return wlanInterface.GetAvailableNetworkList(Wlan.WlanGetAvailableNetworkFlags.IncludeAllManualHiddenProfiles)
+                    .Select(x => x.profileName)
+                    .Where(x => !string.IsNullOrEmpty(x))
+                    .ToArray();
+            }
+        }
+
+        public static void ConnectNetwork(string interfaceName, string profileName)
+        {
+            using (var client = new WlanClient())
+            {
+                var wlanInterface = client.Interfaces.FirstOrDefault(x => x.InterfaceName == interfaceName);
+                if (wlanInterface == null)
+                    return;
+
+                wlanInterface.Connect(Wlan.WlanConnectionMode.Profile, Wlan.Dot11BssType.Any, profileName);
+            }
+        }
+    }
+        public string AccesPoint { get; set; }
+
+        public uint Signal { get; set; }
+        public string BSSType { get; set; }
+        public string MAC { get; set; }
+        public string RSSID { get; set; }
+
+    }
+}
